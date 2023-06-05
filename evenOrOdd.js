a89f455e4351da923e318e676e66f7f4f10f22c6
Ardalber <cochezgaetan07@gmail.com>
2023-06-05 11:50:13 +0200

    try even or odd

---
+++ evenOrOdd.js
@@ -0,0 +1,13 @@
+function evenOrOdd(number) {
+      if (number % 2 === 0) {
+
+        return "is even"
+      }  
+      else {
+
+        return "is odd"
+      }
+         };
+    
+
+         console.log(evenOrOdd(3));
