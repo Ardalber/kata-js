d6532b894db69369c0525c0812696ffb9e3f91e2
Ardalber <cochezgaetan07@gmail.com>
2023-06-05 16:29:29 +0200

    starting remove

---
+++ removeFirstAndLAstChar.js
@@ -0,0 +1,15 @@
+// let word = prompt('entre un mot', "saluts");
+
+
+
+function removeFirstAndLAstLetter (str){
+
+    let enTableau = str.split("");
+console.log(enTableau);
+    let newTableau = enTableau.pop();
+    console.log(newTableau);
+    return newTableau;
+  }
+
+removeFirstAndLAstLetter();
+  
