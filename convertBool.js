0d85f47483bacf1fb626805dc0a1331a645dc8e0
Ardalber <cochezgaetan07@gmail.com>
2023-06-05 20:23:03 +0200

    finishing convert bool

---
+++ convertBool.js
@@ -0,0 +1,11 @@
+function booleanToString(b){
+    if (b === true){
+      return "true"
+    }
+    else return "false"
+  }
+//   autre solution
+
+function booleanToString(b){
+    return b ? 'true' : 'false';
+  }
