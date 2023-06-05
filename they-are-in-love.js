b4a99ccf37810f940016719edc65d8a9b5cee26a
Ardalber <cochezgaetan07@gmail.com>
2023-06-05 20:19:05 +0200

    finishing opposite attract

---
+++ they-are-in-love.js
@@ -0,0 +1,15 @@
+function lovefunc(flower1, flower2){
+    // moment of truth
+    
+    if((flower1 % 2 === 0 && flower2 %2 !== 0)|| (flower1 % 2 !== 0 && flower2 %2 === 0))
+      
+      return true;
+    
+    else return false;
+  }
+
+//   autre solution
+
+function lovefunc(flower1, flower2){
+    return flower1 % 2 !== flower2 % 2;
+  }
